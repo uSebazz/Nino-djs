6e276998201981d914d333bcb5544c99760503ef
uSebazz <cuentaparaelpaisagod@gmail.com>
2022-02-01 13:51:49 -0500

    Cambiando el node :fresh:

--- Src/Data/Utils/Stack/Lavalink.js
+++ Src/Data/Utils/Stack/Lavalink.js
@@ -1,6 +1,6 @@
 module.exports = {
-	lava_host: 'node04.lavalink.eu',
-	lava_pass: 'Raccoon',
+	lava_host: 'disbotlistlavalink.ml',
+	lava_pass: 'LAVA',
 	id: 'c8baf8c5113545b08628a404a08abd58',
 	secret: 'a1b4cb1863b24ca194cf9743104822bc',
 };
