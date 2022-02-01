7cc11e307dfe50df7b40ea421451b4edb9dcac96
uSebazz <cuentaparaelpaisagod@gmail.com>
2022-02-01 13:59:22 -0500

    Otra vez xd

--- Src/Data/Utils/Stack/Lavalink.js
+++ Src/Data/Utils/Stack/Lavalink.js
@@ -1,6 +1,6 @@
 module.exports = {
-	lava_host: 'disbotlistlavalink.ml',
-	lava_pass: 'LAVA',
+	lava_host: 'node02.lavalink.eu',
+	lava_pass: 'Raccoon',
 	id: 'c8baf8c5113545b08628a404a08abd58',
 	secret: 'a1b4cb1863b24ca194cf9743104822bc',
 };
