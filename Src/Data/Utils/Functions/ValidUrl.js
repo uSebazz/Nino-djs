3236aa176d7ef39cca1c0b3d033c9e4cc7b3e58f
uSebazz <90474850+uSebazz@users.noreply.github.com>
2022-02-18 10:02:50 -0500

    Update ValidUrl.js

--- Src/Data/Utils/Functions/ValidUrl.js
+++ Src/Data/Utils/Functions/ValidUrl.js
@@ -2,7 +2,7 @@ var isYouTubeVideoURL = (arg) =>
 	arg.match(/^(http(s)?:\/\/)?(m.)?((w){3}.)?(music.)?youtu(be|.be)?(\.com)?\/.+/);
 
 var isYouTubePlaylistURL = (arg) =>
-	arg.match(/^https?:\/\/(music.)?(www.youtube.com|youtube.com)\/playlist(.*)$/);
+	arg.match(/^https?:\/\/(music.)?(youtube.com)\/playlist(.*)$/);
 
 var isSpotifyURL = (arg) => arg.match(/^(spotify:|https:\/\/[a-z]+\.spotify\.com\/)/);
 
